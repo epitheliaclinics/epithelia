85959d9a81007d65e7b97925ffd8f6fdb6d84b6d
epitheliaclinics <epitheliaclinics@gmail.com>
2026-02-23 17:42:35 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
